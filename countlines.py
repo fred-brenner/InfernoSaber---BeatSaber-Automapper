2a2d1a63a339a661224961716ca8af4f8201fea6
Frederic Brenner <frederic.brenner@tum.de>
2023-06-20 19:52:52 +0200

    add detailed function count

--- countlines.py
+++ countlines.py
@@ -1,14 +1,15 @@
 import os
 import re
+import numpy as np
 
 
 def check_def(line):
     # match def functions at start of line and with optional tab
-    m = re.match("^[\s]*\t?def .*$", line)
+    m = re.match("^[\s]*\t?def (\w+)", line)
     if m:
-        return True
+        return m.group(1)
     else:
-        return False
+        return None
 
 
 def countlines(start, lines=0, header=True, begin_start=None):
@@ -18,6 +19,7 @@ def countlines(start, lines=0, header=True, begin_start=None):
 
     global fct_count
     global script_count
+    self_defined_functions = set()
     for thing in os.listdir(start):
         thing = os.path.join(start, thing)
         if os.path.isfile(thing):
@@ -34,7 +36,9 @@ def countlines(start, lines=0, header=True, begin_start=None):
 
                     cur_fct_count = 0
                     for cur_line in cur_newlines:
-                        if check_def(cur_line):
+                        function_name = check_def(cur_line)
+                        if function_name:
+                            self_defined_functions.add(function_name)
                             cur_fct_count += 1
                     fct_count += cur_fct_count
 
@@ -51,6 +55,53 @@ def countlines(start, lines=0, header=True, begin_start=None):
     return lines
 
 
+def count_function_calls(file_path, self_defined_functions):
+    function_counts = {}
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            matches = re.findall(r'(\w+)\(', line)
+            for match in matches:
+                if match in self_defined_functions:
+                    if match in function_counts:
+                        function_counts[match] += 1
+                    else:
+                        function_counts[match] = 1
+    return function_counts
+
+
+def display_function_usage(project_path):
+    print("\nFunction call counts for self-defined functions:")
+    self_defined_functions = set()
+    for root, dirs, files in os.walk(project_path):
+        for file in files:
+            if file.endswith('.py'):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r') as f:
+                    lines = f.readlines()
+                    for line in lines:
+                        function_name = check_def(line)
+                        if function_name:
+                            self_defined_functions.add(function_name)
+
+    function_counts = {}
+    for root, dirs, files in os.walk(project_path):
+        for file in files:
+            if file.endswith('.py'):
+                file_path = os.path.join(root, file)
+                counts = count_function_calls(file_path, self_defined_functions)
+                for function, count in counts.items():
+                    if function in function_counts:
+                        function_counts[function] += count
+                    else:
+                        function_counts[function] = count
+
+    sorted_counts = sorted(function_counts.items(), key=lambda x: x[1], reverse=True)
+
+    for function, count in sorted_counts:
+        print(f"Function {function:<30} is called {count:>3} times in the project.")
+
+
 if __name__ == '__main__':
     py_path = os.getcwd()
     global fct_count
@@ -59,3 +110,5 @@ if __name__ == '__main__':
     script_count = 0
     countlines(py_path)
     print(f"Total of {script_count} python scripts.")
+
+    display_function_usage(py_path)
