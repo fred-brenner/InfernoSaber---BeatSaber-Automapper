97db1043fda00a179d5549c53c81195d35809496
Frederic Brenner <fredericbrenner98@gmx.de>
2023-10-13 13:30:32 +0200

    did everything

--- tools/config/config.py
+++ tools/config/config.py
@@ -55,8 +55,8 @@ obstacle_max_count = 2  # maximum appearance count for obstacles
 sporty_obstacles = False
 add_slider_flag = True  # add arcs between notes in free areas
 slider_time_gap = [0.5, 10.0]    # time gap in seconds
-slider_probability = 0.8    # [0.1-1.0] with 1 meaning all on
-slider_movement_minimum = 2.2   # minimum movement between notes [0-10]
+slider_probability = 0.9    # [0.1-1.0] with 1 meaning all on
+slider_movement_minimum = 2   # minimum movement between notes [0-10]
 slider_radius_multiplier = 1.0  # [0.5-2.0]
 slider_turbo_start = True   # start slider towards first note
 
