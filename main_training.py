be75a5767c9b814ef0195914db280a2fb7bdb835
Frederic Brenner <frederic.brenner@tum.de>
2023-12-26 18:17:06 +0100

    create main_training.py

--- main_training.py
+++ main_training.py
@@ -19,6 +19,11 @@ input("Adapted the mapper_selection and use_bpm_selection in the config file?\n"
 # run bs_shift / shift.py
 input("Did you run shift.py?")
 
+print("Which trainings do you want to start? Reply with y or n for each model.")
+run_list = input("1. music autoencoder | 2. song mapper | 3. beat generator | 4. lights generator | ")
+if len(run_list) != 4:
+    print("Wrong input format. Exit")
+    exit()
 # create folder if required
 if not os.path.isdir(paths.model_path):
     print(f"Creating model folder: {config.use_mapper_selection}")
@@ -26,17 +31,23 @@ if not os.path.isdir(paths.model_path):
 
 else:
     if len(os.listdir(paths.model_path)) > 1:
-        print("Model folder already available. Exit to change folder in config.")
+        print("Model folder already available. Exit manually to change folder in config.")
         input("Continue with same model folder?")
 
 # you can skip this step
 # run training / train_autoenc_music.py
 # os.system("training/train_autoenc_music.py")
-subprocess.call(['python', './training/train_autoenc_music.py'])
+if run_list[0].lower() == 'y':
+    subprocess.call(['python', './training/train_autoenc_music.py'])
 
 # run training / train_bs_automapper.py
-# os.system(f"{paths.main_path}training/train_bs_automapper.py")
+if run_list[1].lower() == 'y':
+    subprocess.call(['python', './training/train_bs_automapper.py'])
 
 # run beat_prediction / ai_beat_gen.py
+if run_list[2].lower() == 'y':
+    subprocess.call(['python', './beat_prediction/ai_beat_gen.py'])
 
 # run lighting_prediction / train_lighting.py
+if run_list[3].lower() == 'y':
+    subprocess.call(['python', './lighting_prediction/train_lighting.py'])
