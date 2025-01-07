881a706e7e8dd46ca0e900040378e989e305c656
Frederic Brenner <fredericbrenner98@gmx.de>
2025-01-07 21:24:55 +0100

    app update2, fix modifications not saving

--- app_helper/update_dir_path.py
+++ app_helper/update_dir_path.py
@@ -1,15 +1,19 @@
 import re
 
 
-def update_dir_path(file_path, keyword='dir_path', value=''):
+def update_dir_path(file_path, keyword='dir_path', new_value=''):
     """
-    Updates the line containing 'dir_path' in the specified file to the new value.
+    Updates the line containing 'dir_path' in the specified file to the new new_value.
 
     Args:
         file_path (str): Path to the file to be updated.
-        keyword (str): The new path/value to set.
-        value (str): The new content to set.
+        keyword (str): The new path/new_value to set.
+        new_value (str): The new content to set.
     """
+    if new_value.lower() == 'true':
+        new_value = True
+    elif new_value.lower() == 'false':
+        new_value = False
     try:
         # Read the file
         with open(file_path, 'r') as file:
@@ -22,8 +26,8 @@ def update_dir_path(file_path, keyword='dir_path', value=''):
                 # Check if the line contains 'dir_path'
                 if re.match(rf"^\s*{keyword}\s*=", line):
                     # Replace with the new value and ensure a newline is added
-                    file.write(f'{keyword} = "{value}"\n')
-                    print(f"Updated {keyword} in {file_path} to: {value}")
+                    file.write(f'{keyword} = "{new_value}"\n')
+                    print(f"Updated {keyword} in {file_path} to: {new_value}")
                     found_it = True
                 else:
                     file.write(line)
@@ -41,5 +45,5 @@ def update_dir_path(file_path, keyword='dir_path', value=''):
 if __name__ == "__main__":
     file_path = "tools/config/paths.py"
     keyword = "dir_path"
-    value = "/new/directory/path"
-    update_dir_path(file_path, keyword, value)
+    new_value = "/new/directory/path"
+    update_dir_path(file_path, keyword, new_value)
