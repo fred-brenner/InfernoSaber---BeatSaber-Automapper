f4f4d5f7a118957e60a527a107eac5cb6faadf81
Frederic Brenner <fredericbrenner98@gmx.de>
2025-01-13 23:15:28 +0100

    app update4; add info to user input

--- main_app.py
+++ main_app.py
@@ -20,7 +20,43 @@ from tools.utils.huggingface import model_download
 
 data_folder_name = 'Data'
 bs_folder_name = "Beat Saber/Beat Saber_Data/CustomLevels"
-update_check_response = None
+
+
+# update_check_response = None
+# def check_for_updates():
+#     try:
+#         global update_check_response
+#         if update_check_response is None:
+#             repo_owner = "fred-brenner"
+#             # repo_name = "InfernoSaber---BeatSaber-Automapper"
+#             repo_name = "InfernoSaber-App"
+#             github_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases"
+#
+#             response = requests.get(github_url)
+#             latest_version = response.json()[0]['tag_name']
+#             current_version = f"v{config.InfernoSaber_version}"
+#             if latest_version != current_version:
+#                 update_check_response = (f"Version <{latest_version}> is released! Your version is <{config.InfernoSaber_version}>. "
+#                                          f"Please update with the Pinokio Update function on the left.")
+#                 return (f"Version <{latest_version}> is released! Your version is <{config.InfernoSaber_version}>. "
+#                         f"Please update with the Pinokio Update function on the left.")
+#             update_check_response = f"You are up to date ({latest_version})"
+#             return f"You are up to date ({latest_version})"
+#
+#         else:
+#             return update_check_response
+#     except:
+#         print("Could not reach GitHub for update check.")
+#         try:
+#             print(f"Response: {response.json()[0]}")
+#         except:
+#             pass
+#     update_check_response = "Could not reach GitHub for update check."
+#     return "Could not reach GitHub for update check."
+
+
+# call update check once to mitigate rate limiting
+update_status = "Currently not available. Please wait for app5 release in Discord."
 
 
 # # Function to handle folder selection using tkinter
@@ -414,39 +450,6 @@ def set_arc_movement_min(arc_movement_min_value):
     update_dir_path('tools/config/config.py', 'slider_movement_minimum', arc_movement_min_value)
     return
 
-
-def check_for_updates():
-    global update_check_response
-    if update_check_response is None:
-        repo_owner = "fred-brenner"
-        # repo_name = "InfernoSaber---BeatSaber-Automapper"
-        repo_name = "InfernoSaber-App"
-        github_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/releases"
-        try:
-            response = requests.get(github_url)
-            latest_version = response.json()[0]['tag_name']
-        except:
-            print("Could not reach GitHub for update check.")
-            try:
-                print(f"Response: {response.json()[0]}")
-            except:
-                pass
-            update_check_response = "Could not reach GitHub for update check."
-            return "Could not reach GitHub for update check."
-        current_version = f"v{config.InfernoSaber_version}"
-        if latest_version != current_version:
-            update_check_response = (f"Version <{latest_version}> is released! Your version is <{config.InfernoSaber_version}>. "
-                                     f"Please update with the Pinokio Update function on the left.")
-            return (f"Version <{latest_version}> is released! Your version is <{config.InfernoSaber_version}>. "
-                    f"Please update with the Pinokio Update function on the left.")
-        update_check_response = f"You are up to date ({latest_version})"
-        return f"You are up to date ({latest_version})"
-    else:
-        return update_check_response
-
-
-# call update check once to mitigate rate limiting
-update_status = check_for_updates()
 
 # Gradio App Setup Section
 with gr.Blocks() as demo:
