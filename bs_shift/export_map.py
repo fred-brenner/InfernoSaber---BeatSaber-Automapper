7d2aad07d914dc881f8e0342245f2507102b7f9a
Frederic Brenner <fredericbrenner98@gmx.de>
2025-01-28 21:24:03 +0100

    fix mp3 encoding

--- bs_shift/export_map.py
+++ bs_shift/export_map.py
@@ -20,7 +20,8 @@ def check_music_files(files, dir_path):
     song_list = []
     for file_name in files:
         ending = file_name.split('.')[-1]
-        if ending in ['mp3', 'mp4', 'm4a', 'wav', 'aac', 'flv', 'wma']:   # (TODO: test more music formats)
+        # if ending in ['mp3', 'mp4', 'm4a', 'wav', 'aac', 'flv', 'wma']:   # (TODO: test more music formats)
+        if ending in ['mp3', 'mp4', 'm4a']:   # (TODO: test more music formats)
         # if ending in ['NOT_WORKING!']:
             # convert music to ogg format
             output_file = f"{file_name[:-4]}.egg"
